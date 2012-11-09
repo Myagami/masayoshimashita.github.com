851a7b589a66f015e8330dc20f74f995405454da
masayoshi <helloproject.odoru10th@gmail.com>
2012-11-09 20:44:47 +0900

    tsts

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head></head>
+	<body>
+	<h1>Masayoshi Mashita MyPage</h1>
+
+	Twitter:<a href="http://twitter.com/masayoshi_main">@masayoshi_main</a><br>
+
+	Blog::Main : <a href="http://masayoshilogs.hatenablog.com/">/var/log/masayoshi/masayoshi.life.log</a><br>
+	Blog::Simutrans : <a href="http://masatrans-lab-diary.hateblo.jp/">more /home/masayoshi/simutrans/mastrans.log | grep blog</a><br>
+
+	</body>
+
+</html>
